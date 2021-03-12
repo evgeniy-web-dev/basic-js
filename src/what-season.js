77eeae5b81df823299b8f2fdb65d841eb95d418c
evgeniy-web-dev <vem-89@mail.ru>
2021-03-13 00:43:44 +0300

    feat: implement function getSeason (date)

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,18 @@
+// Какой сезон?
+// Задача - реализовать функцию getSeason (date), которая принимает объект Date и возвращает соответствующее ему время года.
+// Время года должно быть строковым.
+
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!";
+  if (Object.prototype.toString.call(date) !== "[object Date]")
+    throw new Error("Error");
+
+  let month = date.getMonth();
+
+  if (month == 11 || month <= 1) return "winter";
+  if (month <= 4) return "spring";
+  if (month <= 7) return "summer";
+  if (month <= 10) return "autumn";
 };

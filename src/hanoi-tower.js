d919cd18ce3c11180353c7ae70e25c24ccd8c57e
evgeniy-web-dev <vem-89@mail.ru>
2021-03-13 00:51:19 +0300

    feat: implement function calculateHanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,15 @@
+// Ханойская башня
+// Реализуйте функцию calculateHanoi, которая принимает параметры diskNumber и turnSpeed.
+// diskNumber - это количество дисков, а TurnSpeed ​​- это скорость перемещения дисков (в оборотах в час).
+// Оба параметра являются числами.
+
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  console.log(turnsSpeed / 3600)
+  const result = {
+    turns: Math.pow(2, disksNumber) - 1,
+    seconds: Math.floor((Math.pow(2, disksNumber) - 1) * (1 / (turnsSpeed / 3600)))
+  };
+  return result
 };

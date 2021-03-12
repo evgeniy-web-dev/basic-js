a98d287f9730da2c3a6dc55569a78c35080ced1e
evgeniy-web-dev <vem-89@mail.ru>
2021-03-13 01:11:41 +0300

    feat: implement DepthCalculator class with calculateDepth method

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,14 @@
+// Рекурсивный калькулятор глубины
+// Задача реализовать класс DepthCalculator с методом calculateDepth, который принимает массив и возвращает его глубину.
+
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+  calculateDepth (arr) {
+    if (!Array.isArray(arr))
+        return 0;
+    return 1 + arr.reduce((depth, item) => Math.max(depth, this.calculateDepth(item)), 0);
+}
+
 };

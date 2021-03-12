94e079c0e63d5c90b5ecda8aa550fa80ccbf5ed8
evgeniy-web-dev <vem-89@mail.ru>
2021-03-13 00:31:40 +0300

    feat: implement function createDreamTeam (members)

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
+// Команда мечты
+// Задача - реализовать функцию createDreamTeam (members),
+// которая возвращает имя созданной команды (строку) на основе имен ее участников (Array).
+
+
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  const team = []
+
+  if (!Array.isArray(members)) {
+    return false
+  }
+
+  for (let name of members) {
+    if (typeof name === 'string') {
+      team.push(name.trim().split('')[0])
+    }
+  }
+  
+  return  team.map((e) => e.toUpperCase()).sort().join('');
 };

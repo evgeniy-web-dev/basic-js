6bbbdd2641405910d2c7abc6602a1cd95edb3abb
evgeniy-web-dev <vem-89@mail.ru>
2021-03-13 00:57:12 +0300

    feat: implement function transform (arr)

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
+// Преобразовать массив
+// Задача - реализовать функцию transform (arr), которая принимает массив и возвращает
+// преобразованный массив на основе управляющих последовательностей, содержащихся в arr.
+
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+  let newArr = arr.slice();
+  for (let i = 0; i < newArr.length; i++) {
+      if(newArr[i] === "--discard-next" && i + 1 < newArr.length) {
+        newArr[i + 1] = "to delete";
+        newArr.splice(i, 1);
+      }
+
+      if(newArr[i] === "--discard-prev" && i - 1 > -1) {
+        newArr[i - 1] = "to delete";
+        newArr.splice(i - 1, 1);
+      }
+
+      if(newArr[i] === "--double-next" && i + 1 < newArr.length && newArr[i + 1] !== "to delete") newArr[i] = newArr [i + 1];
+      if(newArr[i] === "--double-prev" && i - 1 > - 1 && newArr[i - 1] !== "to delete") newArr[i] = newArr[i - 1];
+  }
+
+  let filter = function (item) {
+    if(item === "--discard-next" || item === "--discard-prev" || item === "--double-next" || item === "--double-prev" || item === "to delete") return false;
+    return true;
+  }
+  
+  return newArr.filter(item => filter(item));
 };
